0e94341a7de8e2fd237d99c386a6f10e2d8fbfdf
manu <AmauryBs@users.noreply.github.com>
2019-11-04 14:31:24 +0100

    install mongoose

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const app = express();
 
-app.listen(3000);
-console.log('Running on localhost:3000');
-
 //to access form data
 const bodyParser = require('body-parser');
 
