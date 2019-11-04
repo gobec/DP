76317fc9db689618af54ecd63f98108f32c9a866
marsuraa <tony2parker0@gmail.com>
2019-11-04 14:54:15 +0100

    test

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 
 //to access the database stored in MongoDB
 const mongoose = require('mongoose');
-database = 'mongodb://localhost:27017/DP';
+database = 'mongodb://localhost:27017/DP'; 
 mongoose.connect(database,(err) => {
     if(err)
         throw err;
