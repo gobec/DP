625755142872ae3d9c736e7ffbd116ec77da5831
marsuraa <tony2parker0@gmail.com>
2019-11-15 17:37:27 +0100

    page contact

--- app/src/app/contact/contact.module.ts
+++ app/src/app/contact/contact.module.ts
@@ -3,9 +3,12 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
 
+
 import { IonicModule } from '@ionic/angular';
 
 import { ContactPage } from './contact.page';
+
+
 
 const routes: Routes = [
   {
