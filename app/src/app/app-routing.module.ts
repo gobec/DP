0496433451849e89ef396f105aeda49598f96ad2
marsuraa <tony2parker0@gmail.com>
2020-01-07 09:06:55 +0100

    create page menu

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -6,7 +6,8 @@ const routes: Routes = [
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
   { path: 'carte', loadChildren: './carte/carte.module#CartePageModule' },
   { path: 'contact', loadChildren: './contact/contact.module#ContactPageModule' },
-  { path: 'fournisseurs', loadChildren: './fournisseurs/fournisseurs.module#FournisseursPageModule' },
+  { path: 'fournisseurs', loadChildren: './fournisseurs/fournisseurs.module#FournisseursPageModule' },  { path: 'menu', loadChildren: './menu/menu.module#MenuPageModule' },
+
 ];
 
 @NgModule({
